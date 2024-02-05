00d9e06507023097e2893fe30fafbda26a42d1be
atle <alextle24@gmail.com>
2024-02-05 15:27:52 -0700

    JOIN practice

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,6 @@
+-- Gets the total assignemnt duration for a specific cohort
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
